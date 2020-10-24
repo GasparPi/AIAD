1452d92b454a8e766780d41639b1b1b6295ec4a4
Gaspar99 <pasgas99@hotmail.com>
2020-10-24 15:51:54 +0100

    Update group class

--- meeting_scheduling/src/Group.java
+++ meeting_scheduling/src/Group.java
@@ -1,11 +1,29 @@
+import agents.Employee;
 
 import java.util.HashMap;
 
 public class Group {
 
-    private int id;
+    private final int id;
+    private final String name;
 
-    HashMap<Integer, Integer> employees;
+    HashMap<Integer, Employee> employees;
     HashMap<Integer, Meeting> meetings;
+
+    public Group(int id, String name) {
+        this.id = id;
+        this.name = name;
+
+        this.employees = new HashMap<>();
+        this.meetings = new HashMap<>();
+    }
+
+    public void addEmployee(Employee employee) {
+        this.employees.put(employee.getId(), employee);
+    }
+
+    public void addMeeting(Meeting meeting) {
+        this.meetings.put(meeting.getId(), meeting);
+    }
 
 }
--- meeting_scheduling/src/agents/Employee.java
+++ meeting_scheduling/src/agents/Employee.java
@@ -3,4 +3,15 @@ package agents;
 import jade.core.Agent;
 
 public class Employee extends Agent {
+
+    private final int id;
+
+    public Employee(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return this.id;
+    }
+
 }
