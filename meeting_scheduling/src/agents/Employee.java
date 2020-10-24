c3106d274710e1054ff298c0529c8db4506eb639
SALajes <sofialajes98@gmail.com>
2020-10-24 15:53:48 +0100

    Added Timeslot and EmployeeParser stubs

--- meeting_scheduling/src/agents/Employee.java
+++ meeting_scheduling/src/agents/Employee.java
@@ -3,4 +3,13 @@ package agents;
 import jade.core.Agent;
 
 public class Employee extends Agent {
+    private int id;
+
+    public Employee(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
 }
